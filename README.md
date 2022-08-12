06fa4b5d87881ff7ae3eeb9929deef0e7271179c
charwliu <charwliu@ngboss.com>
2022-08-12 21:15:22 +0800

    init

--- README.md
+++
@@ -1,3 +0,0 @@
-# BeanCountModel
-
-A description of this package.
--- Sources/BeanCountModel/BeanCountModel.swift
+++
@@ -1,6 +0,0 @@
-public struct BeanCountModel {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
