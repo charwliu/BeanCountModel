ad0f7d74dbdfeaafa54bd865f80239dea6af15d2
charwliu <charwliu@ngboss.com>
2022-08-12 17:29:25 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BeanCountModel
+
+A description of this package.
---
+++ Tests/BeanCountModelTests/BeanCountModelTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BeanCountModel
+
+final class BeanCountModelTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BeanCountModel().text, "Hello, World!")
+    }
+}
